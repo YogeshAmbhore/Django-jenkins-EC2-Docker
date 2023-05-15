e85f8637b11d6af3388518827582f564a45d3510
Yogesh Ambhore <yogeshambhore45@gmail.com>
2023-05-15 23:13:31 +0530

    added settings.py file

--- jenkinsec2/settings.py
+++ jenkinsec2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&piulf0zlsm=70ljztv)3ixf7d&ag#qn!(!_$ztdv$m78(enr=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
